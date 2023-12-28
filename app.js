f1e3568564b4bf09495b90bd7531c478af6aa0b1
Burak <burakunv3r@gmail.com>
2023-12-28 13:03:50 +0300

    deploy

--- app.js
+++ app.js
@@ -4,17 +4,17 @@ Promise.all([
   faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
   faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
   faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
-  faceapi.nets.faceExpressionNet.loadFromUri("/models"),
+  faceapi.nets.faceExpressionNet.loadFromUri("/models")
 ]).then(startCamera());
 
 function startCamera() {
   //   return false;
   navigator.getUserMedia(
     {
-      video: {},
+      video: {}
     },
-    (stream) => (video.srcObject = stream),
-    (err) => console.log(err)
+    stream => (video.srcObject = stream),
+    err => console.log(err)
   );
 }
 
@@ -23,7 +23,7 @@ video.addEventListener("play", () => {
   document.body.append(canvas);
   const boxSize = {
     width: video.width,
-    height: video.height,
+    height: video.height
   };
 
   faceapi.matchDimensions(canvas, boxSize);
